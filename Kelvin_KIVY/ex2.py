745e428cb0a87957b464e8846ef795e06ee12b96
KelvinHow <theimportantboytadah@gmail.com>
2018-03-13 13:31:43 +0800

    added boxlayout-kv-example.kv

---
+++ Kelvin_KIVY/ex2.py
@@ -0,0 +1,19 @@
+import kivy
+kivy.require('1.0.6')
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.app import App
+from kivy.lang import Builder
+
+Builder.load_file('boxlayout-kv-example.kv')
+
+class Demo(GridLayout):
+    pass
+
+
+class DemoApp(App):
+    def build(self):
+        return Demo()
+
+if __name__ == '__main__':
+    DemoApp().run()
